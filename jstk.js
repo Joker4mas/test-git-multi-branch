b6c5472c930818dbb807c7d63605432b0fb2a64f
joker4mas <samuelogbaje@gmail.com>
2022-10-21 15:40:31 +0100

    first commit for js-task

---
+++ jstk.js
@@ -0,0 +1,12 @@
+let Full_Name = 'Samuel Ugbaje';
+let CoursesOffered = ['HTML', 'CSS','JavaScript'];
+
+console.log('My name is :' + Full_Name);
+console.log('Courses Offered are ' + CoursesOffered);
+
+console.log(CoursesOffered.length);
+for (let i=0; i <=200; i ++){
+    if (i % 3){
+        
+    }
+}
---
+++ let Full_Name = 'Samuel Ugbaje';.js
@@ -0,0 +1,14 @@
+let Full_Name = 'Samuel Ugbaje';
+let CoursesOffered = ['HTML', 'CSS','JavaScript'];
+
+console.log('My name is :' + Full_Name);
+console.log('Courses Offered are ' + CoursesOffered);
+
+
+if (CourseOffered.length % 3 ===0){
+    for (let i =0; i<=200; i++){
+        if(i % 3 ==0){
+            console.log(i);
+        }
+    }
+}
